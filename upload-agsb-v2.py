1ebd76b641a7a590304af0f5574292812a9ac009
xujw3 <kingxujw3@gmail.com>
2025-06-06 22:48:33 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -31,11 +31,11 @@ DEBUG_LOG = INSTALL_DIR / "python_debug.log"
 CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的域名
 
 # ====== 全局可配置参数（可直接在此处修改） ======
-USER_NAME = "kkddytd"         # 用户名
-UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
-PORT = 49999                   # Vmess端口，留空或0则自动生成
-DOMAIN = "free.testlrj.ddns-ip.net"                   # 域名，留空则自动获取
-CF_TOKEN = "eyJhIjoiODBmMjY5ZmQ1N2QzNzNiMmMzZTBkODc4ODg1NWM5MzIiLCJ0IjoiZmVhMzBmODUtOGY5OC00ZTVmLTkyZTktMmU2OTk2M2E1YzUyIiwicyI6Ik4yTmlZemxpTlRjdE5UVm1PQzAwTjJZekxXRmpORGt0TVdVNE5HUmtORGN3TldObSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
+USER_NAME = "kingxujw"         # 用户名
+UUID = "0b8d6f00-fabd-45b9-a319-95375ad1d493"                     # UUID，留空则自动生成
+PORT = 10086                   # Vmess端口，留空或0则自动生成
+DOMAIN = "vless.kingxx.ggff.net"                   # 域名，留空则自动获取
+CF_TOKEN = "eyJhIjoiNDJlN2NiYTIxNmY2YWI5NzEzZDQyODUyMWYzM2ZiNTEiLCJ0IjoiN2ViMmM5YjAtMzAzZS00MDBkLTk2MTEtZjhhN2IwMmFlN2M3IiwicyI6IlkyRXpZVGhsWm1ZdFpUY3dNUzAwWTJVMExXRmxZbVV0T1RrNU1EY3lNVE15TjJOaCJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
 # 添加命令行参数解析
